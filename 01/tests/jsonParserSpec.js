2d45375389a36c3f91e13f992f2ee80caa9264a6
youngiggy <youngiggy@gmail.com>
2017-03-28 01:03:33 +0900

    it returns an object

--- 01/tests/jsonParserSpec.js
+++ 01/tests/jsonParserSpec.js
@@ -1,4 +1,19 @@
 describe('jsonParser', function () {
-    it('does nothing', function () {
+    'use strict';
+
+    var validJsonStrings;
+
+    beforeEach(function() {
+        validJsonStrings = [
+            '{"a":3}',
+            '{"a":3, "b":{"d":5}}',
+            '{"a":3, "b":{"d":{"e":5}}}'
+        ];
+    });
+
+    it('returns an object with valid string', function () {
+        var parsedObj = jsonParser(validJsonStrings[0]);
+        expect(typeof parsedObj).toEqual('object');
+        expect(parsedObj).toEqual({});
     });
 });
