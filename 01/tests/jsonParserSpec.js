f7930424d773344da9e6d8b9e740909b708ca3fa
youngiggy <youngiggy@gmail.com>
2017-03-28 23:33:55 +0900

    간단한 예제부터 하나씩 실행해보기 위해 단순화

--- 01/tests/jsonParserSpec.js
+++ 01/tests/jsonParserSpec.js
@@ -6,18 +6,11 @@ describe('jsonParser', function () {
 
     beforeEach(function() {
         parser = jsonParser();
-        validJsonStrings = [
-            '{"a":3}',
-            '{"a":3, "b":{"d":5}}',
-            '{"a":3, "b":{"d":{"e":5}}}'
-        ];
     });
 
     it('returns an object with valid string', function () {
-        validJsonStrings.forEach(function (str) {
-            var parsedObj = parser(str);
-            expect(typeof parsedObj).toEqual('object');
-            expect(parsedObj).toEqual({});
-        });
+        var parsedObj = parser('');
+        expect(typeof parsedObj).toEqual('object');
+        expect(parsedObj).toEqual({});
     });
 });
